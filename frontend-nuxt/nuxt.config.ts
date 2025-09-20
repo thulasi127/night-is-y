b4e6743b48979ff280d83f262069b3c4c90cd724
your_github_username <59294422+thulasi127@users.noreply.github.com>
2025-09-20 13:43:49 -0400

    Updating logo on bookmark

--- frontend-nuxt/nuxt.config.ts
+++ frontend-nuxt/nuxt.config.ts
@@ -1,4 +1,16 @@
 export default defineNuxtConfig({
   css: ['~/assets/css/global.css', '~/assets/css/scroll-fix.css'],
   compatibilityDate: '2025-09-03',
+
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/Image/NIY-logos/niy-logo-1.png'
+        }
+      ]
+    }
+  }
 })
