8b3f7fac43f5fb9bb08a4cda3b54dd578255df44
your_github_username <59294422+thulasi127@users.noreply.github.com>
2026-01-15 16:39:47 -0500

    Fix Sanity deploy by moving autoUpdates to deployment config

--- backend-sanity/sanity.cli.ts
+++ backend-sanity/sanity.cli.ts
@@ -3,11 +3,9 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'a9t1mosy',
-    dataset: 'production'
+    dataset: 'production',
   },
-  /**
-   * Enable auto-updates for studios.
-   * Learn more at https://www.sanity.io/docs/cli#auto-updates
-   */
-  autoUpdates: true,
+  deployment: {
+    autoUpdates: false,
+  },
 })
